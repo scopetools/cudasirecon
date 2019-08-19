c5db65bb7dd781890fdcbbee405ce725068d295c
Talley Lambert <talley.lambert@gmail.com>
2019-08-19 18:00:18 -0400

    updates for mac build

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -6,11 +6,27 @@ rm -rf build
 mkdir build
 cd build
 
-export LDFLAGS="-L${PREFIX}/lib"
-# export CXXFLAGS="-L${PREFIX}/lib"
-export CC=x86_64-conda_cos6-linux-gnu-gcc
-export CXX=x86_64-conda_cos6-linux-gnu-g++
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib" ..
+if [ `uname` == Linux ]; then
+    export LDFLAGS="-L${PREFIX}/lib"
+    # export CXXFLAGS="-L${PREFIX}/lib"
+    export CC=x86_64-conda_cos6-linux-gnu-gcc
+    export CXX=x86_64-conda_cos6-linux-gnu-g++
+    cmake .. \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+        -DCMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib"
+fi
+
+if [ `uname` == Darwin ]; then
+    export CC=gcc
+    export CXX=g++
+    cmake .. \
+        -Wno-dev \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+        -DCMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib"
+fi
+
 
 make
 make install
