ebab1bea871692ba197a7ca9d03354b7232012f4
Talley Lambert <talley.lambert@gmail.com>
2019-08-19 20:36:33 -0400

    try built in gcc

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -9,13 +9,13 @@ cd build
 if [ `uname` == Linux ]; then
     export LDFLAGS="-L${PREFIX}/lib"
     # export CXXFLAGS="-L${PREFIX}/lib"
-    export CC=x86_64-conda_cos6-linux-gnu-gcc
-    export CXX=x86_64-conda_cos6-linux-gnu-g++
+    # export CC=x86_64-conda_cos6-linux-gnu-gcc
+    # export CXX=x86_64-conda_cos6-linux-gnu-g++
     cmake .. \
         -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="${PREFIX}"
-#        -DCMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib" \
+        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+        -DCMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib"
 #        -DCMAKE_NO_BUILTIN_CHRPATH:BOOL=ON
 fi
 
