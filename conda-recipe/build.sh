9a11c4098f7f8a7f56c33f5e78eb019c4fefe9b5
Talley Lambert <tlambert03@gmail.com>
2019-08-21 08:29:03 -0400

    build.sh hopefully working for linux/mac with makeotf

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -4,23 +4,63 @@
 # cudatoolkit, whereas the mac version assumes you have it installed in the local environment
 # AND, for mac: the environmental variable CUDA_VERSION must be set, e.g.:
 # export CUDA_VERSION=10.1 && conda build conda-recipe
+rm -rf build
+mkdir build
+cd build
 
 if [ `uname` == Linux ]; then
     export LDFLAGS="-L${PREFIX}/lib"
-    export CC=x86_64-conda_cos6-linux-gnu-gcc
-    export CXX=x86_64-conda_cos6-linux-gnu-g++
+    export CC=$PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc
+    export CXX=$PREFIX/bin/x86_64-conda_cos6-linux-gnu-g++
+    # export CC=gcc
+    # export CXX=g++
+    PLATFORM=linux64
+
+    #CUDA_TOOLKIT_ROOT_DIR="/usr/local/cuda-${CUDA_VERSION}"
+    #CUDA_LIB_DIR="${CUDA_TOOLKIT_ROOT_DIR}"/lib64
+
+    cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib"
+#    -D CUDA_TOOLKIT_ROOT_DIR="${CUDA_TOOLKIT_ROOT_DIR}" \
+
 fi 
 
 if [ `uname` == Darwin ]; then
     export CC=gcc
     export CXX=g++
+    PLATFORM=darwin64
+    CUDA_TOOLKIT_ROOT_DIR="/Developer/NVIDIA/CUDA-${CUDA_VERSION}"
+    CUDA_LIB_DIR="${CUDA_TOOLKIT_ROOT_DIR}"/lib
+
+    cmake .. \
+    -D CUDA_TOOLKIT_ROOT_DIR="${CUDA_TOOLKIT_ROOT_DIR}" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib"
+
 fi
 
-rm -rf build
-mkdir build
-cd build
 
-# build makeotf
+make -j4
+make install
+ln -s ${PREFIX}/bin/cudaSireconDriver ${PREFIX}/bin/cudasirecon
+ln -s ${PREFIX}/bin/cudaSireconDriver ${PREFIX}/bin/sirecon
+
+#if [ `uname` == Linux ]; then
+#    cp "${CUDA_LIB_DIR}"/libcufft.*.so "${PREFIX}"/lib/
+#fi
+
+
+if [ `uname` == Darwin ]; then
+    cp "${CUDA_LIB_DIR}"/libcufft.*.dylib "${PREFIX}"/lib/
+    install_name_tool -add_rpath @executable_path/../lib "${PREFIX}/bin/makeotf"
+fi
+
+cd "${SRC_DIR}/build"
+
+# install fftw
 wget http://www.fftw.org/fftw-2.1.5.tar.gz
 tar -zxvf fftw-2.1.5.tar.gz
 cd fftw-2.1.5
@@ -28,44 +68,11 @@ cd fftw-2.1.5
 make
 make install
 
+# build makeotf
 $CC "${SRC_DIR}/otf/makeotf.c" \
-    -I"${SRC_DIR}/IVE/darwin64/INCLUDE" \
+    -I"${SRC_DIR}/IVE/${PLATFORM}/INCLUDE" \
     -I"${PREFIX}/include" \
-    -L"${SRC_DIR}/IVE/darwin64/LIB" \
+    -L"${SRC_DIR}/IVE/${PLATFORM}/LIB" \
     -L"${PREFIX}/lib" \
-    -limlib -lsrfftw -lsfftw \
+    -limlib -lsrfftw -lsfftw -lm \
     -o "${PREFIX}/bin/makeotf" 
-
-cd "${SRC_DIR}/build"
-
-if [ `uname` == Linux ]; then
-    cmake .. \
-        -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-        -DCMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib"
-#        -DCMAKE_NO_BUILTIN_CHRPATH:BOOL=ON
-fi
-
-if [ `uname` == Darwin ]; then
-    install_name_tool -add_rpath @executable_path/../lib "${PREFIX}/bin/makeotf"
-
-    echo "CUDA_VERSION: ${CUDA_VERSION}"
-    CUDA_TOOLKIT_ROOT_DIR="/Developer/NVIDIA/CUDA-${CUDA_VERSION}"
-    echo "CUDA_TOOLKIT_ROOT_DIR: ${CUDA_TOOLKIT_ROOT_DIR}"
-    cmake .. \
-        -Wno-dev \
-        -D CUDA_TOOLKIT_ROOT_DIR="${CUDA_TOOLKIT_ROOT_DIR}" \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-        -DCMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib"
-fi
-
-make -j4
-make install
-ln -s ${PREFIX}/bin/cudaSireconDriver ${PREFIX}/bin/cudasirecon
-ln -s ${PREFIX}/bin/cudaSireconDriver ${PREFIX}/bin/sirecon
-
-if [ `uname` == Darwin ]; then
-    cp "${CUDA_TOOLKIT_ROOT_DIR}"/lib/libcufft.*.dylib "${PREFIX}"/lib/
-fi
