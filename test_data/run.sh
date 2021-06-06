33bc3d3b9b857938834d4fd3868e25fd4ca357b9
Talley Lambert <talley.lambert@gmail.com>
2021-06-06 17:13:48 -0400

    fix run script

--- test_data/run.sh
+++ test_data/run.sh
@@ -2,13 +2,10 @@
 # makeotf psf.dv otf.otf -angle -0.804300 -ls 0.2035 -na 1.4 -nimm 1.515 -fixorigin 3 20
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-cd ../"$parent_path"
-
-APP=./cmake_build/cudaSirecon/cudaSireconDriver
+APP="$parent_path/../cmake_build/cudaSirecon/cudaSireconDriver"
 
 # test TIFF
-$APP $parent_path/raw.tif $parent_path/proc.tif $parent_path/otf.otf --xyres=0.08 --zres=0.125 -c $parent_path/config
+$APP $parent_path raw $parent_path/psf.tif --xyres=0.08 --zres=0.125 --zresPSF=0.125 -c $parent_path/config
 
 # test MRC
-# $APP $parent_path/raw.dv $parent_path/proc.dv $parent_path/otf.otf -c $parent_path/config
+$APP $parent_path/raw.dv $parent_path/proc.dv $parent_path/otf.otf -c $parent_path/config
