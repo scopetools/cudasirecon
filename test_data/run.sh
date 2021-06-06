9f069d5593a4c448eed14c64ef2ec21a4607ed30
Talley Lambert <talley.lambert@gmail.com>
2021-06-06 17:01:43 -0400

    work on linux again

--- test_data/run.sh
+++ test_data/run.sh
@@ -6,4 +6,9 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd ../"$parent_path"
 
 APP=./cmake_build/cudaSirecon/cudaSireconDriver
-$APP $parent_path/raw.dv $parent_path/proc.dv $parent_path/otf.otf -c $parent_path/config
+
+# test TIFF
+$APP $parent_path/raw.tif $parent_path/proc.tif $parent_path/otf.otf --xyres=0.08 --zres=0.125 -c $parent_path/config
+
+# test MRC
+# $APP $parent_path/raw.dv $parent_path/proc.dv $parent_path/otf.otf -c $parent_path/config
