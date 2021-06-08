cb9ca4bfe7a8a0360e492b8652e633247220dd91
Talley Lambert <talley.lambert@gmail.com>
2021-06-08 13:50:09 -0400

    update build

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 rm -rf cmake_build
 
-if [ ! -d "src/IVE" ] 
-then
-    wget src/ https://www.dropbox.com/s/2twvw0go3dr3aim/IVE.zip
-    unzip -o -d src IVE.zip
-    rm IVE.zip
-fi
+# if [ ! -d "src/IVE" ] 
+# then
+#     wget src/ https://www.dropbox.com/s/2twvw0go3dr3aim/IVE.zip
+#     unzip -o -d src IVE.zip
+#     rm IVE.zip
+# fi
 
 
 mkdir cmake_build
@@ -15,11 +15,12 @@ cd cmake_build
 # ... probably a better way
 CXXFLAGS="-Wfatal-errors -fPIC -pipe -isystem ${CONDA_PREFIX}/include"
 
-cmake $CMAKE_ARGS \
+cmake ../src $CMAKE_ARGS \
     -DCMAKE_C_COMPILER=$CC \
     -DCMAKE_CXX_COMPILER=$CXX \
     -DCMAKE_BUILD_TYPE=Release \
-    ../src
+    -DBUILD_MRC=OFF
+    
 
 make -j 4
 # make install
