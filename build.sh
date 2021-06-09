0bd1e98d2a895bf5a192e344a2cee928a585a1d5
Talley Lambert <talley.lambert@gmail.com>
2021-06-09 09:14:49 -0400

    remove cuda_add_library...

--- build.sh
+++ build.sh
@@ -11,17 +11,8 @@ fi
 mkdir cmake_build
 cd cmake_build
 
-# This is just to remove the -std= from CXXFLAGS added by conda
-# ... probably a better way
-CXXFLAGS="-Wfatal-errors -Wno-deprecated-declarations -fPIC -pipe -isystem ${CONDA_PREFIX}/include"
-
-cmake ../src $CMAKE_ARGS \
-    -DCMAKE_C_COMPILER=$CC \
-    -DCMAKE_CXX_COMPILER=$CXX \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_MRC=OFF
-    # -DBUILD_OTF_VIEWER=ON \
-    
+CXXFLAGS="$CXXFLAGS -Wfatal-errors -Wno-deprecated-declarations"
+cmake ${CMAKE_ARGS} -DBUILD_MRC=OFF -DCMAKE_BUILD_TYPE=Release ../src
 
 make -j 4
 # make install
