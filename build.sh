f68fcc4a1fa1c75efca8a44125110f683ce45209
Talley Lambert <talley.lambert@gmail.com>
2021-06-08 22:21:24 -0400

    cleanup on FFTW and option to make otfviewer

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 rm -rf cmake_build
 
-# if [ ! -d "src/IVE" ] 
-# then
-#     wget src/ https://www.dropbox.com/s/2twvw0go3dr3aim/IVE.zip
-#     unzip -o -d src IVE.zip
-#     rm IVE.zip
-# fi
+if [ ! -d "src/IVE" ] 
+then
+    wget src/ https://www.dropbox.com/s/2twvw0go3dr3aim/IVE.zip
+    unzip -o -d src IVE.zip
+    rm IVE.zip
+fi
 
 
 mkdir cmake_build
@@ -13,13 +13,14 @@ cd cmake_build
 
 # This is just to remove the -std= from CXXFLAGS added by conda
 # ... probably a better way
-CXXFLAGS="-Wfatal-errors -fPIC -pipe -isystem ${CONDA_PREFIX}/include"
+CXXFLAGS="-Wfatal-errors -Wno-deprecated-declarations -fPIC -pipe -isystem ${CONDA_PREFIX}/include"
 
 cmake ../src $CMAKE_ARGS \
     -DCMAKE_C_COMPILER=$CC \
     -DCMAKE_CXX_COMPILER=$CXX \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_MRC=OFF
+    # -DBUILD_OTF_VIEWER=ON \
     
 
 make -j 4
