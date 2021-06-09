73ca09247f83ec5f06e64d65f7488e9047e81db9
Talley Lambert <talley.lambert@gmail.com>
2021-06-09 10:41:22 -0400

    updates and formatting

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ mkdir cmake_build
 cd cmake_build
 
 CXXFLAGS="$CXXFLAGS -Wfatal-errors -Wno-deprecated-declarations"
-cmake ${CMAKE_ARGS} -DBUILD_MRC=OFF -DCMAKE_BUILD_TYPE=Release ../src
+cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release ../src
 
 make -j 4
 # make install
